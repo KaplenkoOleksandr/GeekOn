03f7a8a60bc9f2168d1fc4e55f313dd4d74e88fb
Maria Lisova <romashka.124578369@gmail.com>
2022-02-15 23:28:30 +0200

    Add models

---
+++ Geekon/Models/Subtask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geekon.Models
+{
+    public class Subtask
+    {
+        public int SubtaskId { get; set; }
+        public string SubtaskName { get; set; }
+        public TaskStatus Status { get; set; }
+        //public ICollection<User> Users { get; set; }
+        public DateTime Date { get; set; }
+        public string Comment { get; set; }
+    }
+
+    public enum TaskStatus
+    { 
+        ToDo,
+        InProgress,
+        Finished,
+        Blocked
+    }
+
+}
